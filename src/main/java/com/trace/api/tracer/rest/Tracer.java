677a185592db21812a7ba88780381f0d1a202bb2
BH13434 <raj.kiran@macys.com>
2020-01-20 11:07:27 +0530

    request body has been added

--- src/main/java/com/trace/api/tracer/rest/Tracer.java
+++ src/main/java/com/trace/api/tracer/rest/Tracer.java
@@ -9,6 +9,7 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,16 +17,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class Tracer {
 
 	@GetMapping("/trace")
-	public String Greeting(HttpServletRequest request) throws IOException{
-		
+	public String Greeting(HttpServletRequest request,@RequestBody Object requestBody) throws IOException{
+
 			Enumeration<String> enm = request.getHeaderNames();
 			System.out.println("Headers");
 			while(enm.hasMoreElements()){
 				String header = enm.nextElement();
 			System.out.println(header +":"+ request.getHeader(header));
 			}
-
-		return "success1";
+			System.out.println("---------------------------");
+			System.out.println("Request Body");
+			System.out.println(requestBody);
+		return "success";
 	}
 }
 
